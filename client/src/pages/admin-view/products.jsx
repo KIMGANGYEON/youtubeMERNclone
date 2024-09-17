88baa4a3fdbda92117074302c9a2d309b01d7c2c
KIM GANG YEON <rkddus3177@naver.com>
2024-09-17 19:54:41 +0900

    하루에 조금씩

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -49,8 +49,8 @@ const AdminProducts = () => {
             <SheetTitle>Add New Product</SheetTitle>
           </SheetHeader>
           <ProductImageUpload
-            file={imageFile}
-            setFile={setImageFile}
+            imageFile={imageFile}
+            setImageFile={setImageFile}
             uploadedImageUrl={uploadedImageUrl}
             setUploadedImageUrl={setUploadedImageUrl}
           />
